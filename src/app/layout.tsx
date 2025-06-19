cfd826df712e739cfe4b8d77a922c09ca89229fb
feelsuegood <129813530+feelsuegood@users.noreply.github.com>
2025-06-19 15:03:38 +1000

    update font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,23 +1,34 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+// import { Geist, Geist_Mono } from "next/font/google";
+import { Plus_Jakarta_Sans, DM_Sans } from "next/font/google";
 import { ThemeProvider } from "@/components/theme-provider";
 // These styles apply to every route in the application
 import "./globals.css";
 
 // Fonts
 //TODO: change that suits the design
-const geistSans = Geist({
-  variable: "--font-geist-sans",
+// const geistSans = Geist({
+//   variable: "--font-geist-sans",
+//   subsets: ["latin"],
+// });
+
+// const geistMono = Geist_Mono({
+//   variable: "--font-geist-mono",
+//   subsets: ["latin"],
+// });
+
+const mainFont = Plus_Jakarta_Sans({
   subsets: ["latin"],
+  variable: "--font-main",
 });
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
+const bodyFont = DM_Sans({
   subsets: ["latin"],
+  variable: "--font-body",
 });
 
 export const metadata: Metadata = {
-  title: "Feelsuegood's Portfolio",
+  title: "🎁 Feelsuegood's Portfolio",
   description: "The collection of Feelsuegood's personal projects",
   icons: "/favicon.ico",
 };
@@ -30,7 +41,7 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <body
-        className={`${geistSans.variable} ${geistMono.variable} bg-background px-8 py-10 text-foreground antialiased md:px-20 md:py-20`}
+        className={`${mainFont.variable} ${bodyFont.variable} bg-background px-8 py-10 text-foreground antialiased md:px-20 md:py-20`}
       >
         <ThemeProvider
           attribute="class"
